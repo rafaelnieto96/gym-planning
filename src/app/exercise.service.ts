8ecc015271619e3e7349e5a205e0a693e8c7d088
Rafael Nieto <radikan.dev@gmail.com>
2024-01-10 19:15:13 +0100

    ADD environment

--- src/app/exercise.service.ts
+++ src/app/exercise.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ExerciseService {
 
-    private apiUrl = 'http://localhost:8080/api/exercises';
+    private apiUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://rafaelnieto96.github.io/gym/api/exercises'
+  };
+  
