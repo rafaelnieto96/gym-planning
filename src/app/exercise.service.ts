f52dffc26856d4651ff66e15c7c77acf34b78e49
Rafael Nieto <radikan.dev@gmail.com>
2024-01-14 13:18:51 +0100

    changed api url

--- src/app/exercise.service.ts
+++ src/app/exercise.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ExerciseService {
 
-    private apiUrl = environment.apiUrl;
+    private apiUrl = 'https://rafaelnieto96.github.io/gym/api/exercises';
 
     constructor(private http: HttpClient) { }
 
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://rafaelnieto96.github.io/gym/api/exercises'
-  };
-  
